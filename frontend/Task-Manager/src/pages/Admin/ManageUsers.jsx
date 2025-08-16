e06f7632f140467fed9a0af85d03760f2be931b6
Assil Benkhadija <assylbenkhdija@gmail.com>
2025-08-16 19:07:48 +0200

    User Card UI and downloading task reports

--- frontend/Task-Manager/src/pages/Admin/ManageUsers.jsx
+++ frontend/Task-Manager/src/pages/Admin/ManageUsers.jsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DashboardLayout from '../../components/Layouts/DashboardLayout';
+import { API_PATHS } from '../../utils/apiPaths';
+import axiosInstance from '../../utils/axiosinstance';
+import { LuFileSpreadsheet } from 'react-icons/lu';
+import UserCard from '../../components/Cards/UserCard';
 const ManageUsers = () => {
+
+    const [allUsers, setAllUsers] = useState([]);
+
+    const getAllUsers = async () => {
+        try {
+            const response = await axiosInstance.get(API_PATHS.USERS.GET_ALL_USERS);
+            if (response.data?.length > 0) {
+                setAllUsers(response.data);
+            }
+        } catch (error) {
+            console.error("Error fetching users:", error);
+        }
+    };
+
+    //download task report
+    const handleDownloadReport = async () => {
+        try {
+            const response = await axiosInstance.get(API_PATHS.REPORTS.EXPORT_USERS, {
+                responseType: 'blob'
+            });
+
+            //create a URL for the blob
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'users_details.xlsx'); //or any other extension
+            document.body.appendChild(link);
+            link.click();
+            link.parentNode.removeChild(link);
+            window.URL.revokeObjectURL(url);
+        } catch (error) {
+            console.error("Error downloading report:", error);
+            toast.error("Failed to download report details. Please try again later.");
+        }
+    };
+    useEffect(() => {
+        getAllUsers();
+
+        return () => { };
+    }, [])
+
     return (
-        <div>ManageUsers</div>
+        <DashboardLayout activeMenu="Team Members">
+            <div className='mt-5 mb-5'>
+                <div className='flex md:flex-row md:items-center justify-between'>
+                    <h2 className='text-xl md:text font-medium'>
+                        Team Members
+                    </h2>
+
+                    <button className='flex md:flex download-btn' onClick={handleDownloadReport}>
+                        <LuFileSpreadsheet className='text-lg' />
+                        Download Report
+                    </button>
+                </div>
+
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-4'>
+                    {allUsers?.map((user) => (
+                        <UserCard key={user._id} userInfo={user} />
+                    ))}
+                </div>
+            </div>
+        </DashboardLayout>
     )
 }
 export default ManageUsers;
